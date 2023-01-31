c7e5cdcfed2fef86c8b7510c20ab4367f6c87d23
Arpit Gupta <Arpitgupta1866@gmail.com>
2023-01-31 15:52:15 +0530

    Quiz commit

---
+++ quiz.java
@@ -0,0 +1,17 @@
+import javax.sound.sampled.SourceDataLine;
+
+public class quiz {
+    public static void main(String[] args) {
+       /*  int y = 7;
+        int x = ++y*8;
+        System.out.println(x);*/
+        char ch = 'a';
+        System.out.println(++ch);
+
+    }
+       
+    
+    
+    
+    
+}
